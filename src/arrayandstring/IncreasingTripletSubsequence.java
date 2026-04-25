95416069f6786a1c8d19fffc962c7ca8cffe8615
Yohan Ishara <yohanishara01@gmail.com>
2026-04-25 19:04:12 +0530

    Check availability Increasing Triplet Subsequence

---
+++ src/arrayandstring/IncreasingTripletSubsequence.java
@@ -0,0 +1,38 @@
+package arrayandstring;
+
+/*Given an integer array nums, return true if there exists a triple of indices (i, j, k)
+such that i < j < k and nums[i] < nums[j] < nums[k]. If no such indices exists, return false.*/
+public class IncreasingTripletSubsequence {
+
+
+/*Input: nums = [1,2,3,4,5]
+Output: true
+Explanation: Any triplet where i < j < k is valid.
+
+Input: nums = [5,4,3,2,1]
+Output: false
+Explanation: No triplet exists.
+
+Input: nums = [2,1,5,0,4,6]
+Output: true
+Explanation: One of the valid triplet is (1, 4, 5), because nums[1] == 1 < nums[4] == 4 < nums[5] == 6.*/
+    public static void main(String[] args) {
+        int[] nums ={2,1,5,0,4,6};
+        System.out.println(increasingTriplet(nums));
+    }
+
+    public static boolean increasingTriplet(int[] nums) {
+        int first = Integer.MAX_VALUE;
+        int second = Integer.MAX_VALUE;
+        for (int num : nums) {
+            if (num <= first) {
+                first = num;
+            } else if (num <= second) {
+                second = num;
+            }else {
+                return true;
+            }
+        }
+        return false;
+    }
+}
